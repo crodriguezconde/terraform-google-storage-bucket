4f015027a5b41d72baf39b595a7906527adb4e97
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-26 19:22:11 +0100

    Fixed dynamic behaviour when enabling logging

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# This conditions checks whether the user would like to use logging configuration in
-# their Cloud Storage bucket. If set to false, the whole logging block will be ignored.
-
-locals {
-  logging = var.logging_configuration["is_logging_enabled"] == false ? {} : var.logging_configuration
-}
-
-
 resource "google_storage_bucket" "storage_bucket" {
 
   name          = var.storage_bucket_name
@@ -15,8 +7,15 @@ resource "google_storage_bucket" "storage_bucket" {
   versioning {
     enabled = var.is_versioning_enabled
   }
+  # There is currently no easy way to handle null blocks within Terraform, so a hacky
+  # way to do it is to use the dynamic expression only if logging is enabled.
   dynamic "logging" {
-    for_each = local.logging
+    # for_each needs an iterable list to loop through in order to create as many 
+    # "dynamic" objects as items are in the list. Using [*] we ensure that the 
+    # iterable list is always 1, as we are only creating 1 config per each bucket in 
+    # case the logging configuration is enabled
+
+    for_each = var.logging_configuration["is_logging_enabled"] == false ? [] : var.logging_configuration[*]
     content {
       log_bucket        = var.logging_configuration["log_destination_storage_bucket"]
       log_object_prefix = var.logging_configuration["log_object_prefix"]
