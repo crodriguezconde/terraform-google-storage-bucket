f7d102bf279bc9ee22d45826bbe954770af36ea0
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-26 17:04:47 +0100

    Fixed issue when declaring logging and versioning blocks.

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "google_storage_bucket" "storage_bucket" {
   force_destroy = var.is_force_destroy_enabled
   location      = var.storage_bucket_location
   storage_class = var.storage_class
-  versioning = {
+  versioning {
     enabled = var.is_versioning_enabled
   }
-  logging = {
+  logging {
     log_bucket        = var.log_destination_storage_bucket
     log_object_prefix = var.log_object_prefix
   }
