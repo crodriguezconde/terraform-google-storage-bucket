4d8fb8dd385e20d6bfd151c1f2451004af8b7bdd
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-26 22:35:34 +0100

    Added main.tf & variables.tf for bucket ACLs.

---
+++ modules/storage_bucket_acl/main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket_access_control" "bucket_access_control" {
+	bucket = var.storage_bucket_name
+	role = var.storage_bucket_access_role
+	entity = var.storage_bucket_entity
+}
