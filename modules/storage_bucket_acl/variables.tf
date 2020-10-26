24b3d1393e8d190846e267f2e234229637a9188c
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-26 22:41:49 +0100

    Formatted files

--- modules/storage_bucket_acl/variables.tf
+++ modules/storage_bucket_acl/variables.tf
@@ -1,18 +1,18 @@
 variable "storage_bucket_name" {
-	description = "The name of the Cloud Storage bucket."
-	type = string
+  description = "The name of the Cloud Storage bucket."
+  type        = string
 }
 
 variable "storage_bucket_access_role" {
-	description = "The access permission for the entity. Possible values are OWNER, READER & WRITER."
-	type = string
-	validation {
-		condition = contains(["OWNER","READER","WRITER"],var.storage_bucket_access_role)
-		error_message = "The access permissio for an entity should be OWNER, READER or WRITER."
-	}
+  description = "The access permission for the entity. Possible values are OWNER, READER & WRITER."
+  type        = string
+  validation {
+    condition     = contains(["OWNER", "READER", "WRITER"], var.storage_bucket_access_role)
+    error_message = "The access permissio for an entity should be OWNER, READER or WRITER."
+  }
 }
 
 variable "storage_bucket_entity" {
-	description = "The entity holding the permission, in one of the following forms: user-userId user-email group-groupId group-email domain-domain project-team-projectId allUsers allAuthenticatedUsers"
-	type = string
+  description = "The entity holding the permission, in one of the following forms: user-userId user-email group-groupId group-email domain-domain project-team-projectId allUsers allAuthenticatedUsers"
+  type        = string
 }
