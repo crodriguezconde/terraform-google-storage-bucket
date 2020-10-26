ab84868772ae3cdf6ba1265dd41b3257115394a3
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-26 16:47:10 +0100

    Added basic structure for a new Google Cloud Storage bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+resource "google_storage_bucket" "storage_bucket" {
+
+  name          = var.storage_bucket_name
+  force_destroy = var.is_force_destroy_enabled
+  location      = var.storage_bucket_location
+  storage_class = var.storage_class
+  versioning = {
+    enabled = var.is_versioning_enabled
+  }
+  logging = {
+    log_bucket        = var.log_destination_storage_bucket
+    log_object_prefix = var.log_object_prefix
+  }
+  requester_pays = var.is_requester_pays_enabled
+}
